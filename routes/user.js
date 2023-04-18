70578b3402fd271960002bbe89ae0e6b7116d697
unknown <developerakshayjadhav@gmail.com>
2023-04-18 12:27:38 +0530

    new

--- routes/user.js
+++ routes/user.js
@@ -10,4 +10,6 @@ router.get('/user/:id', user.getUserById);
 router.post('/user/login', user.loginUser);
 router.post('/user/signup', user.postSignup);
 
+router.put('/user/:id',user.updateUserById);
+
 module.exports = router;
