7d4c1f1faa32ea8b949bd3d84c867ed5fd9991f5
unknown <developerakshayjadhav@gmail.com>
2023-04-18 13:13:53 +0530

    new

--- models/SubAdmin.model.js
+++ models/SubAdmin.model.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const subAdminSchema = new Schema({
-    name:{
-        type: String,
-        required: [true, 'Name is required'],
-        unique: true,
-    },
+   
     email:{
         type: String,
         required: [true, 'Password is required'],
